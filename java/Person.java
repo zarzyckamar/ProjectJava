d45ec8e2bc68e0554a4aa08a04a940fc88d0c9f3
zarzyckamar <Mojageneracja21>
2018-10-29 15:22:34 +0100

    scanner

---
+++ java/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    public String imie;
+    public String nazwisko;
+    public String pesel;
+
+    public Person (String imie, String nazwisko, String pesel)
+    {
+        this.imie=imie;
+        this.nazwisko=nazwisko;
+        this.pesel=pesel;
+    }
+}
